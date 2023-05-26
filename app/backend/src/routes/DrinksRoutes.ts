37dcbdce577db9f35eaecb9c9deb6a44baca1dc3
Gabrielv0id <gabriel1998.fonseca@gmail.com>
2023-05-26 13:59:33 -0300

    implements routes and seed for drinks

--- app/backend/src/routes/DrinksRoutes.ts
+++ app/backend/src/routes/DrinksRoutes.ts
@@ -1,11 +1,11 @@
 import { Router } from 'express';
 import DrinksController from '../controllers/DrinksController';
-import queryStringMiddleware from '../middlewares/queryString.middleware';
 
 const DrinksRouter = Router();
 
-DrinksRouter.get('/drinks/name', queryStringMiddleware, (req, res) => DrinksController.getAll(req, res));
-DrinksRouter.get('/drinks/letter', queryStringMiddleware, (req, res) => DrinksController.findByFirstLetter(req, res));
+DrinksRouter.get('/drinks/name', (req, res) => DrinksController.getAll(req, res));
+DrinksRouter.get('/drinks/letter', (req, res) => DrinksController.findByFirstLetter(req, res));
 DrinksRouter.get('/drinks/random', (req, res) => DrinksController.getRandom(req, res));
+DrinksRouter.get('/drinks/categories', (req, res) => DrinksController.getAllDrinksCategories(req, res));
 
 export default DrinksRouter;
